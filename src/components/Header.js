ac66d8ea2049e81c50ba5f9a94d547e034325cba
AndyAtari <andrewsalteri@gmail.com>
2021-05-12 17:31:17 -0400

    Styled header. WIP

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,28 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <div className="header">
+          
             <div className="home-link">
                 <Link to="/">
                     <h1>Home</h1>
                 </Link>
             </div>
-            <div>
+            
+                <div>
                 <Link to="/about">
                     <p>about</p>
                 </Link>
+                </div>
+                <div>
                 <Link to="/portfolio">
                     <p>portfolio</p>
                 </Link>
+                </div>
+                <div>
                 <Link to="/blog">
                     <p>blog</p>
                 </Link>
+                
             </div>
         </div>
     )
