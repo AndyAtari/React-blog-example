63559925a862d42efdd8e59d62fedcca57b67553
AndyAtari <andrewsalteri@gmail.com>
2021-05-12 15:29:10 -0400

    Added Header component to src.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="app-container">
+      <Header /> 
       <h1>My Test Blog</h1> 
     </div>
   )
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="home-link">
+                <h1>Home</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header; 
